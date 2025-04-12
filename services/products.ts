a990af766315a7aa88335c69be58a8a7eb234936
Shketik_R <ruslans3101@gmail.com>
2025-04-12 13:06:46 +0300

    init 4

--- services/products.ts
+++ services/products.ts
@@ -1,8 +1,21 @@
 import { Product } from "@prisma/client"
-import { axiosInstance } from "./instance"
+// import { axiosInstance } from "./instance"
 import { ApiRouts } from "./constants";
 
+// export const getAll = async (): Promise<Product[]> => {
+//   const {data} = await axiosInstance.get<Product[]>(ApiRouts.PRODUCTS);
+//   return data;
+// }
+
+
 export const getAll = async (): Promise<Product[]> => {
-  const {data} = await axiosInstance.get<Product[]>(ApiRouts.PRODUCTS);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/${ApiRouts.PRODUCTS}`);
+
+  if (!response.ok) {
+    console.log(response);
+  }
+
+  const data: Product[] = await response.json();
+  
   return data;
 }
